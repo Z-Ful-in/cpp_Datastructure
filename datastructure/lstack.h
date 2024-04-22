36331d55a82349ef461375d50c23980078301c04
ZIHIKA <1918938521@qq.com>
2024-04-22 16:37:59 +0800

    implement lstack

--- datastructure/lstack.h
+++ datastructure/lstack.h
@@ -3,4 +3,21 @@
 #include "lvector.h"
 
 template <typename T> class lstack:public lvector<T>{
+public:
+	void push(const T& item);
+	T pop();
+	T& top()const;
 };
+
+template<typename T> void lstack<T>::push(const T& item){
+	lvector<T>::push_back(item);
+}
+
+template<typename T> T lstack<T>::pop(){
+	return lvector<T>::remove(lvector<T>::_size - 1);
+}
+
+template<typename T> T& lstack<T>::top() const
+{
+	return lvector<T>::elem[lvector<T>::_size - 1];
+}

c377fe4585de47e545a536c1f5abc263e316e2dc
ZIHIKA <1918938521@qq.com>
2024-05-08 22:46:10 +0800

    modify as inline version

--- datastructure/tools.h
+++ datastructure/tools.h
@@ -1,15 +1,15 @@
 #pragma once
-template<typename T> void lswap(T& a, T& b) {
+template<typename T> inline void lswap(T& a, T& b) {
 	T temp = a;
 	a = b;
 	b = temp;
 }
 
-template<typename T> T lmax(T a, T b) {
+template<typename T> inline T lmax(T a, T b) {
 	return (a > b) ? a : b;
 }
 
-int lstrlen(const char* str) {
+inline int lstrlen(const char* str) {
 	int res = 0;
 	while (*str != '\0') {
 		res++;
